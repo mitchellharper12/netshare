1231071899f57652743a4fcf8c8032f6280bdd1c
Mitchell Harper <mitchwharper@gmail.com>
2020-08-12 21:02:55 -0700

    Add initial org tests and validations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,5 @@
 class Organization < ApplicationRecord
   has_many :users
+  validates_presence_of :title
+  validates :title, length: { in: 4..30 }
 end
--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class OrganizationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without title" do
+    organization = Organization.new
+    assert_not organization.save
+  end
+
+  test "should not allow empty title" do
+    organization = Organization.new
+    organization.update(title: "123")
+    assert_not organization.save
+  end
 end
